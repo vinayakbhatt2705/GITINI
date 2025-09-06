a40024066abfb42f7b0449bdf8b2512ef1ffe53b
vinayakbhatt2705 <vinayakbhatt2705@gmail.com>
2025-09-06 12:42:53 +0530

    Add files via upload

--- backend/server.mongo.js
+++ backend/server.mongo.js
@@ -3,12 +3,15 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const vendorLocationRoutes = require('./routes/vendorLocation');
 
-// Routes
 const profileRoutes = require("./routes/profiles.mongo");
 const userRoutes = require("./routes/users.mongo.js");
 const enquiryRoutes = require("./routes/enquiries.mongo");
 const videoRoutes = require("./routes/videos.mongo");
+const profilesadd = require("./routes/profiles.add.mongo.js");
+
+
 const app = express();
 const PORT = process.env.PORT || 4500;
 
@@ -17,26 +20,35 @@ const { authenticateJWT, authenticateRole } = require("./routes/auth.js");
 const User = require("./models/User.mongo.js"); // mongoose model for users
 
 const SECRET_KEY = "mysecret";
+
+
+
+
+
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 //app.use('/sms', smsRoutes);
 
+
+
 // ✅ MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log("✅ Connected to MongoDB"))
+.then(() => console.log("✅ Connected to MongoDB Database"))
 .catch(err => console.error("❌ MongoDB connection error:", err));
 
 // ✅ Routes
+app.use('/vendors-location', vendorLocationRoutes);
 app.use("/profiles", profileRoutes);
 app.use("/users", userRoutes);
 app.use("/enquiries", enquiryRoutes);
 app.use("/videos", videoRoutes);
-
+app.use("/profiles/add", profilesadd);
 
 app.post("/login", async (req, res) => {
 
@@ -71,7 +83,10 @@ console.log("Generated token:", token);
   }
 });
 
-app.use(express.static(path.join(__dirname, 'dist/frontend')));
+
+
+
+/*app.use(express.static(path.join(__dirname, 'dist/frontend')));
 
 // catch-all for Angular routes
 app.get('*', (req, res) => {
@@ -80,9 +95,9 @@ app.get('*', (req, res) => {
 
 app.listen(process.env.PORT, () => {
   console.log('Server running at' + process.env.MONGO_URI);
-});
-/*Start server
-const PORT = 3000;
+});*/
+/*Start server*/
+//const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
-});*/
+});
